6311fa221a1457d0ecb5be977e3b95dbae96197c
KroLik99 <isabelle.ebach@gmail.com>
2022-07-22 15:43:22 +0200

    onclick attribute vs addEventListener

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ Log out "Button clicked!" when the user clicks the SAVE input button
  */
 
 function clicked(){
-    console.log("Button clicked!")
+    console.log("Button clicked from onclick attribute!")
 }
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function () {
+    console.log("Button clicked from addEvenListener.")
+})
