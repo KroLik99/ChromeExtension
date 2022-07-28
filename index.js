fa6ac44042c683f09ca42fcf886b2b26c3f09565
KroLik99 <73555216+KroLik99@users.noreply.github.com>
2022-07-28 11:39:11 +0200

    inputEl.value =""

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function (){
-    myLeads =[]
+    inputEl.value =""
     myLeads.push(inputEl.value)
     renderLeads()
     }
