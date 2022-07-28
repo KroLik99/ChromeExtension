f61b3abe7745638c5338e3250b7454ad50ffa94f
KroLik99 <isabelle.ebach@gmail.com>
2022-07-28 09:14:55 +0200

    Added innerHTML to display bullet points <li>

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ inputBtn.addEventListener("click", function (){
 )
 /* Log out the items in the myLeads array using a for loop and add to ul*/
 for(let i =0; i<myLeads.length;i++){
-    ulEl.textContent += myLeads[i] + " "
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
 }
