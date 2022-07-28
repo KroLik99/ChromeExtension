e469efe6244055c00bdcb4a52279e86d336976ed
KroLik99 <isabelle.ebach@gmail.com>
2022-07-28 08:55:19 +0200

    Get value from input field

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-/*
-Create 2 variables:
-myLeads -> should be assigned to an empty array
-inputEl -> should be assigned to the text input
- */
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
-
 const inputBtn = document.getElementById("input-btn");
 
+//push the value "www.awesomelead.com" to myArray when the input button is clicked
+
 inputBtn.addEventListener("click", function (){
+    myLeads.push(inputEl.value)
     console.log("Clicked")
-    console.log(inputEl)
+    console.log("The input is: " + inputEl)
+    console.log(myLeads)
     }
 )
-
