dc0d301abebbfbd5b73ce82a837ad02c85ab2a4d
KroLik99 <isabelle.ebach@gmail.com>
2022-07-29 08:49:29 +0200

    Open link in new tab and added changing link

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["a","b","c"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
@@ -7,12 +7,15 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function (){
     myLeads.push(inputEl.value)
-    console.log("Clicked")
-    console.log("The input is: " + inputEl)
-    console.log(myLeads)
+    inputEl.value =""
+    renderLeads()
     }
 )
-/* Log out the items in the myLeads array using a for loop and add to ul*/
-for(let i =0; i<myLeads.length;i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
+
+function renderLeads(){
+    let listItems =""
+    for(let i =0; i<myLeads.length;i++){
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] +"</a></li>"
+    }
+    ulEl.innerHTML += listItems
 }
